a33b96f7531619e23ca32e2fa86b11465871533b
marujan <janmaru@gmail.com>
2021-12-03 02:04:33 +0100

    day 1

---
+++ AdventOfCode2021/Models/Day11.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AdventOfCode2021.Models
+{
+    public class Day11
+    {
+        private int countIncreases;
+        private int? previous;
+        public Day11()
+        {
+            previous = null;
+        }
+
+        public void Parse(string action)
+        {
+            var value = Convert.ToInt32(action);
+            if (!previous.HasValue) 
+                countIncreases = 0; 
+            else 
+                if (previous < value)
+                    countIncreases += 1; 
+            previous = value;
+        }
+
+        public int GetCount()
+        {
+            return countIncreases;
+        }
+    }
+}
